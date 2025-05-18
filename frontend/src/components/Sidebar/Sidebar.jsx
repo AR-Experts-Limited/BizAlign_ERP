9ee7ef255b40c45888d09f2e1accee39b05ad164
Sanjaykumar Ramachandran <ramachandransanjaykumar@gmail.com>
2025-05-18 15:58:47 +0100

    sidebar tooltip added

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,44 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink } from "react-router-dom";
-import { FcSurvey } from "react-icons/fc";
-import { FcBusinessman } from "react-icons/fc";
-import { FcCalculator } from "react-icons/fc";
-import { FcSms } from "react-icons/fc";
-import { FcClock } from "react-icons/fc";
-import { FcPrint } from "react-icons/fc";
-import { FcRules } from "react-icons/fc";
-import { FcSalesPerformance } from "react-icons/fc";
-import { FcLineChart } from "react-icons/fc";
-
-import { IoIosNotificationsOutline } from "react-icons/io";
-import { TbLayoutDashboard } from "react-icons/tb";
-import { IoFileTrayFullOutline } from "react-icons/io5";
-import { PiClockClockwiseLight } from "react-icons/pi";
-import { CiCircleMinus } from "react-icons/ci";
-import { BsCalendarDate } from "react-icons/bs";
-import { FiUsers, FiX, FiMenu } from "react-icons/fi";
-import { HiOutlineListBullet } from "react-icons/hi2";
-import { CiCirclePlus } from "react-icons/ci";
-import { MdAccessTime } from "react-icons/md";
-import { BiMoneyWithdraw } from "react-icons/bi";
-import { BiPrinter } from "react-icons/bi";
-import { BiLineChart } from "react-icons/bi";
+import * as Tooltip from '@radix-ui/react-tooltip';
+import { FcSurvey, FcBusinessman, FcCalculator, FcSms, FcClock, FcPrint, FcRules, FcSalesPerformance, FcLineChart } from "react-icons/fc";
 import { BiStation } from "react-icons/bi";
 
 const Sidebar = ({ sidebarIsOpen }) => {
-
     const [delayedPointerEnabled, setDelayedPointerEnabled] = useState(false);
 
     useEffect(() => {
         let timeout;
-        if (!sidebarIsOpen) {
-            timeout = setTimeout(() => {
-                setDelayedPointerEnabled(false);
-            }, 300);
-        } else {
-            setDelayedPointerEnabled(true);
-        }
-
+        setDelayedPointerEnabled(true);
+        timeout = setTimeout(() => {
+            setDelayedPointerEnabled(false);
+        }, 300);
         return () => clearTimeout(timeout);
     }, [sidebarIsOpen]);
 
@@ -47,13 +21,13 @@ const Sidebar = ({ sidebarIsOpen }) => {
         { path: "/manage-personnels", name: "Manage Personnels", icon: <FcBusinessman size={20} /> },
         { path: "/rate-card", name: "Rate Cards", icon: <FcCalculator size={22} /> },
         { path: "/notifications", name: "Notifications", icon: <FcSms size={20} /> },
-        { path: "/planner", name: "Schedule Planner", icon: <img src='/sidebar_icons/schedule.png' className='h-5 w-5' /> },
+        { path: "/planner", name: "Schedule Planner", icon: <img src='/sidebar_icons/schedule.png' className='h-5 w-5' alt="schedule" /> },
         { path: "/live-operations", name: "Live Operations", icon: <BiStation className='text-blue-500' size={20} /> },
-        { path: "/rota", name: "Rota", icon: <img src='/sidebar_icons/clipboard.png' className='h-5 w-5' /> },
+        { path: "/rota", name: "Rota", icon: <img src='/sidebar_icons/clipboard.png' className='h-5 w-5' alt="rota" /> },
         { path: "/working-hours", name: "Working Hours", icon: <FcClock size={20} /> },
-        { path: "/installments", name: "Installments", icon: <img src='/sidebar_icons/loan.png' className='h-5 w-5' /> },
-        { path: "/deductions", name: "Deductions", icon: <img src='/sidebar_icons/tax.png' className='h-5 w-5' /> },
-        { path: "/incentives", name: "Incentives", icon: <img src='/sidebar_icons/benefit.png' className='h-5 w-5' /> },
+        { path: "/installments", name: "Installments", icon: <img src='/sidebar_icons/loan.png' className='h-5 w-5' alt="installments" /> },
+        { path: "/deductions", name: "Deductions", icon: <img src='/sidebar_icons/tax.png' className='h-5 w-5' alt="deductions" /> },
+        { path: "/incentives", name: "Incentives", icon: <img src='/sidebar_icons/benefit.png' className='h-5 w-5' alt="incentives" /> },
         { path: "/manage-summary", name: "Manage Summary", icon: <FcRules size={20} /> },
         { path: "/manage-payments", name: "Manage Payments", icon: <FcSalesPerformance size={20} /> },
         { path: "/print-invoices", name: "Print Invoices", icon: <FcPrint size={20} /> },
@@ -62,32 +36,47 @@ const Sidebar = ({ sidebarIsOpen }) => {
 
     return (
         <div
-            className={`h-full bg-neutral-300/80 dark:bg-dark-3 shadow-[inset_-2px_0_4px_rgba(0,0,0,0.1)]  overflow-auto transition-all duration-300 origin-left
-                        ${sidebarIsOpen ? 'w-40 md:w-60' : 'w-0 md:w-17'}
-                    `}>
+            className={`h-full bg-neutral-300/80 dark:bg-dark-3 shadow-[inset_-2px_0_4px_rgba(0,0,0,0.1)] overflow-auto transition-all duration-300 origin-left
+        ${sidebarIsOpen ? 'w-40 md:w-60' : 'w-0 md:w-17'}
+      ${delayedPointerEnabled ? 'pointer-events-none' : ''}`}
+        >
             <div className='mb-12'>
-                <div className="flex flex-col h-full flex-nowrap gap-5 md:gap-3 m-2 justify-around  dark:bg-dark-3 dark:border-dark-4 ">
-                    {menuItems.map((item) => (
-
-                        <NavLink
-                            key={item.path}
-                            to={item.path}
-                            className={({ isActive }) =>
-                                `relative p-1 px-2 text-sm md:p-3 md:px-4 flex items-center gap-1 rounded-md overflow-hidden
-                    text-black dark:hover:bg-dark-5  hover:text-primary-500 hover:bg-primary-300/30 hover:shadow-md dark:text-white whitespace-nowrap group
-                    ${isActive ? "bg-primary-700 border border-primary-500  text-white shadow-lg" : ""}
-                    ${delayedPointerEnabled ? '' : 'justify-center'}`
-                            }
-                        >
-                            <div className='flex gap-1 md:gap-4' >
-                                <div className='w-5 h-5'>{item.icon}</div>
-                                <div className={`${delayedPointerEnabled ? 'block' : 'hidden'}`}>{item.name}</div>
-                            </div>
-
-                        </NavLink>
-
-                    ))}
-
+                <div className="flex flex-col h-full gap-5 md:gap-3 m-2">
+                    <Tooltip.Provider delayDuration={500}>
+                        {menuItems.map((item) => (
+                            <Tooltip.Root key={item.path}>
+                                <Tooltip.Trigger asChild>
+                                    <div>
+                                        <NavLink
+                                            to={item.path}
+                                            className={({ isActive }) =>
+                                                `relative p-1 text-sm md:p-3 md:px-4 flex items-center gap-1 rounded-md overflow-hidden
+                       text-black dark:hover:bg-dark-5 hover:text-primary-500 hover:bg-primary-300/30 hover:shadow-md dark:text-white whitespace-nowrap group
+                       ${isActive ? "bg-primary-700 border border-primary-500 text-white shadow-lg" : ""}`
+                                            }
+                                        >
+                                            <div className='flex gap-1 md:gap-4 items-center'>
+                                                <div className='w-5 h-5'>{item.icon}</div>
+                                                <div >{item.name}</div>
+                                            </div>
+                                        </NavLink>
+                                    </div>
+                                </Tooltip.Trigger>
+                                {!sidebarIsOpen && !delayedPointerEnabled && (
+                                    <Tooltip.Portal>
+                                        <Tooltip.Content
+                                            side="right"
+                                            sideOffset={0}
+                                            className="hidden md:block z-50 rounded bg-gray-600 text-white text-xs px-2 py-1 animate-fade-in"
+                                        >
+                                            {item.name}
+                                            <Tooltip.Arrow className="fill-gray-600" />
+                                        </Tooltip.Content>
+                                    </Tooltip.Portal>
+                                )}
+                            </Tooltip.Root>
+                        ))}
+                    </Tooltip.Provider>
                 </div>
             </div>
         </div>
